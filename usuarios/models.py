7f69f7e0c6229482ae24d36f9ce74312b6ad9efb
fede <federicomon23@gmail.com>
2024-03-18 23:43:22 -0300

    modelo usuario
    se agrega el modelo usuario

--- usuarios/models.py
+++ usuarios/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Usuario(models.Model):
+    id = models.AutoField(primary_key=True)
+    mail = models.EmailField(unique=True)
+    password = models.CharField(max_length=255)
+    activo = models.BooleanField(default=True)
+
+    def str(self):
+        return f'{self.mail} - Activo: {bool(self.activo)}'
